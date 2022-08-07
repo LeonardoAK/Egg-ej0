bc2ef580c1a3b718308a247527a945e658da8d83
LeonardoAK <leonardo.acostak@gmail.com>
2022-08-07 16:22:12 -0300

    Tercer commit

--- src/ej0/pkg9/_guia6/Ej09_guia6  v1.java
+++ src/ej0/pkg9/_guia6/Ej09_guia6  v1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ej0.pkg9._guia6;
 
 import java.util.Scanner;
